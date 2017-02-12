ce502d6330dd693aa1db3573d3ae1f24a9b31b76
Gopal Shah <simplecooldude1@gmail.com>
2017-02-11 19:33:05 -0800

    add babel, and fix up code generator for scripts

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
+var path = require("path");
+var fs = require("fs");
 module.exports = {
   compilers: {
     pug: function(str) {
@@ -5,6 +7,14 @@ module.exports = {
     },
     stylus: function(str) {
       return require("stylus")(str).render();
+    },
+    babel: function(str) {
+      var babel = require("babel-core");
+      var babelConfigPath = path.join(process.cwd(), '.babelrc');
+      var babelOptions = fs.existsSync(babelConfigPath) ? json.parse(fs.readFileSync(babelConfigPath, 'utf-8')) : {
+        presets: ['es2015-nostrict']
+      }
+      return babel.transform(str, babelOptions).code;
     }
   }
 }
